3c19b73424811a1cbebae7ccf71c3fdef379b717
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2021-12-29 19:19:49 -0600

    Add project files.

---
+++ EulersTotientFunction.cs
@@ -0,0 +1,25 @@
+﻿using FrooxEngine;
+using FrooxEngine.LogiX;
+using BaseX;
+
+namespace TotientMod
+{
+    [NodeName("Eulers Totient Function")]
+    [Category(new string[] { "LogiX/Operators" })]
+
+    public sealed class EulersTotientFunction : LogixNode
+    {
+        public readonly Input<int> input;
+        public readonly Output<int> output;
+
+        protected override void OnEvaluate()
+        {
+            int result = 1;
+            for (int i = 2; i < input.EvaluateRaw(); i++)
+                if (MathX.GreatestCommonDivisor(i, input.EvaluateRaw()) == 1)
+                    result++;
+            output.Value = result;
+        }
+    }
+}
+
